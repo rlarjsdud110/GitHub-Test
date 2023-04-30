af3eaf412abbc5e31ddf80e315ce4fd9eb95f42e
GeonYoung Kim <rlarjsdud110@naver.com>
2023-04-30 19:38:46 +0900

    first commit

---
+++ src/GitTest/GitTest.java
@@ -0,0 +1,13 @@
+package GitTest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
